680da599d042acdda702e1fc3965b55672238317
admin <thondeboer@me.com>
2021-02-17 23:40:40 -0800

    Moved 'python-lib/illumina/ilmn_platform' to 'python-lib'

---
+++ python-lib/ilmn_platform/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ilmn_platform.api.credentials_api import CredentialsApi
+from ilmn_platform.api.custom_credentials_api import CustomCredentialsApi
