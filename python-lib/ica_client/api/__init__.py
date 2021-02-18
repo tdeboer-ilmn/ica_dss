181da42293f25bed05854fd00c406c2e82fdda96
admin <thondeboer@me.com>
2021-02-17 23:40:27 -0800

    Moved 'python-lib/illumina/ica_client' to 'python-lib'

---
+++ python-lib/ica_client/api/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ica_client.api.accounts_api import AccountsApi
+from ica_client.api.files_api import FilesApi
+from ica_client.api.folders_api import FoldersApi
+from ica_client.api.health_api import HealthApi
+from ica_client.api.projects_api import ProjectsApi
+from ica_client.api.regions_api import RegionsApi
+from ica_client.api.subscriptions_api import SubscriptionsApi
+from ica_client.api.task_runs_api import TaskRunsApi
+from ica_client.api.task_versions_api import TaskVersionsApi
+from ica_client.api.tasks_api import TasksApi
+from ica_client.api.tokens_api import TokensApi
+from ica_client.api.usages_api import UsagesApi
+from ica_client.api.volume_configurations_api import VolumeConfigurationsApi
+from ica_client.api.volumes_api import VolumesApi
+from ica_client.api.workflow_runs_api import WorkflowRunsApi
+from ica_client.api.workflow_signals_api import WorkflowSignalsApi
+from ica_client.api.workflow_versions_api import WorkflowVersionsApi
+from ica_client.api.workflows_api import WorkflowsApi
+from ica_client.api.workgroups_api import WorkgroupsApi
