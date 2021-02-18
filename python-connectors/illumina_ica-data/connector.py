d3bba021adc339b93f3d60ed55a2ed58a09c39dc
admin <thondeboer@me.com>
2021-02-17 21:04:27 -0800

    Edited file 'python-connectors/illumina_ica-data/connector.py'

--- python-connectors/illumina_ica-data/connector.py
+++ python-connectors/illumina_ica-data/connector.py
@@ -3,7 +3,7 @@
 # import the base class for the custom dataset
 from six.moves import xrange
 from dataiku.connector import Connector
-
+import os
 """
 A custom Python dataset is a subclass of Connector.
 
